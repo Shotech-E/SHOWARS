2cd95ed52cb1413f043c6142bf9880c9f3a2493e
Shotech <showars1990@gmail.com>
2024-11-12 06:38:00 +0100

    changes made checkout session

--- frontend/src/pages/shop/OrderSummary.jsx
+++ frontend/src/pages/shop/OrderSummary.jsx
@@ -36,7 +36,7 @@ const OrderSummary = () => {
     }
 
     const response = await fetch(
-      `http://localhost:5000/api/orders/create-checkout-session`,
+      `${getBaseUrl()}/api/orders/create-checkout-session`,
       {
         method: "POST",
         headers: headers,
